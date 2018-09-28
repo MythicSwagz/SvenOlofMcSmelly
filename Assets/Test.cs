ac56a008949b7131a26372c3faba1945ea4c6d3f
david.pohlman <david.pohlman@elev.ga.lbs.se>
2018-09-28 15:02:52 +0200

    hejsan

---
+++ Assets/Test.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public bool ifBool;
+    public int value;
+    public int userValue;
+    public int points = 10;
+    public int playerLife = 100;
+    public int DragonHP;
+    public int DragonATK;
+    public int secondD;
+    private int maxDMG;
+    private int minDMG;
+    public int CriticalDamage;
+    private int HLbaseValue = 50;
+    public int YKey;
+    private int OldHL;
+    public int HKey;
+    public int Score;
+    public int startValue;
+    // Use this for initialization
+    void Start()
+    {
+        //if (ifBool == true)
+        //{
+        //    print("Ja");
+        //}
+        //else
+        //{
+        //    print("Nej");
+        //}
+        DragonHP = DragonHP + Random.Range(100, 151);
+        minDMG = Random.Range(1, 11);
+        maxDMG = Random.Range(20, 31);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Uppgift5();
+        // != inte lika med
+        // == lika med
+        // > större än
+        // < mindre än
+        // >= större eller lika med
+        // <= mindre eller lika med
+        // && och (shift6)
+        // || eller (altgr <)
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Uppgift 1a");
+            userValue = userValue + 2;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Uppgift 1b");
+            userValue = userValue / 2;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print(userValue);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Uppgift 2a");
+            points = points + Random.Range(1, 7);
+            points = points - Random.Range(1, 7);
+            Debug.Log(points);
+            if (points >= 20)
+            {
+                print("Congrats!");
+            }
+            if (points <= 0)
+            {
+                print("Game Over!");
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Uppgift 3");
+            DragonATK = Random.Range(1, 3);
+            if (DragonATK == 1)
+            {
+                playerLife = playerLife - Random.Range(10, 21);
+            }
+            DragonHP = DragonHP - Random.Range(minDMG, maxDMG);
+            if (playerLife <= 0)
+            {
+                print("Dragon Wins");
+            }
+            if (DragonHP <= 0)
+            {
+                print("Player Wins");
+            }
+            print(string.Format(
+                 "DragonHP:" + DragonHP));
+            print(string.Format(
+                "PlayerHP:" + playerLife));
+            secondD = Random.Range(1, 11);
+            if (secondD == 1)
+            {
+                DragonHP = DragonHP * 2;
+            }
+            CriticalDamage = Random.Range(1, 21);
+            if (CriticalDamage == 1)
+            {
+                DragonHP = 0;
+            }
+
+        }
+
+
+
+
+
+
+
+        //if (value == 5)
+        //{
+        //    print("Kanske");
+        //}
+        //else if (value == 4)
+        //{
+        //    print("Vet ej");
+        //}
+        //else if (value == 3)
+        //{
+        //    print("Tyst");
+        //}
+        //else if (value == 2)
+        //{
+        //    print("Ja");
+        //}
+        //else
+        //{
+        //    print("Nej");
+        //}
+    }
+
+
+    void Uppgift5()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            OldHL = YKey;
+            YKey = Random.Range(1, 101);
+            if (YKey <= OldHL)
+            {
+                print("You Lose");
+                startValue = 50;
+            }
+            if (YKey >= OldHL)
+            {
+                print("Keep Going");
+                Score += 1;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            OldHL = YKey;
+            YKey = Random.Range(1, 101);
+            if (YKey <= OldHL)
+            {
+                print("Keep Going");
+                Score += 1;
+            }
+            if (YKey >= OldHL)
+            {
+                print("You Lose");
+                startValue = 50;
+            }
+
+        }
+    }
+
+}
+
---
+++ Assets/Uppgift.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uppgift : MonoBehaviour
+{
+    public string sentence1;
+    public string sentence2;
+    public string sentence3;
+    public int number;
+    public int triangleBase;
+    public int angle;
+    public int pieces;
+    public string username;
+    public int radius;
+    public float damage;
+    public int life;
+    public int demons;
+    public float minDamage;
+    public int maxDamage;
+    public float count;
+    public float countMulti;
+    private float playerMaxLife = 1000;
+    private float playerResist = 0.2f;
+    private float enemyDamage = 100;
+    // Use this for initialization
+    void Start()
+    {
+        Uppgift1();
+        Uppgift2();
+        Uppgift3();
+        Uppgift4();
+        Uppgift5();
+        Uppgift5a();
+        Uppgift6();
+        Uppgift7();
+        Uppgift8();
+        Uppgift8a();
+        Uppgift8b();
+        Uppgift8c();
+        Uppgift9();
+        Uppgift10();
+        Uppgift11();
+    }
+    void Uppgift2()
+    {
+        Debug.Log(string.Format("Uppgift 2: {0}", sentence1));
+        Debug.Log(sentence2);
+        Debug.Log(sentence3);
+    }
+    void Uppgift1()
+    {
+        Debug.Log(string.Format(
+            "Uppgift 1: {0}",
+            (963f * 421f) - (175463f / 87f)));
+    }
+    void Uppgift3()
+    {
+        Debug.Log(string.Format("Uppgift 3: {0}", Mathf.Pow(number, 2)));
+        Debug.Log(Mathf.Sqrt(number));
+    }
+    void Uppgift4()
+    {
+        Debug.Log(string.Format("Uppgift 4: {0}", triangleBase * 8 / 2));
+    }
+    void Uppgift5()
+    {
+        Debug.Log(string.Format("Uppgift 5: {0}", 360f / angle));
+    }
+    void Uppgift5a()
+    {
+        Debug.Log(string.Format("Uppgift 5: {0}", 360f / pieces));
+    }
+    void Uppgift6()
+    {
+        Debug.Log(string.Format("Uppgift 6: {0}", string.Format("Farmer " + username + " of Säve")));
+    }
+    void Uppgift7()
+    {
+        Debug.Log(string.Format("Uppgift 7: {0}", 4 * Mathf.PI * Mathf.Pow(radius, 3) / 3 * 2978));
+    }
+    void Uppgift8()
+    {
+        Debug.Log(string.Format("Uppgift 8: {0}", 890000 / damage));
+    }
+    void Uppgift8a()
+    {
+        Debug.Log(string.Format("Uppgift 8a: {0}", life / damage));
+    }
+    void Uppgift8b()
+    {
+        Debug.Log(string.Format("Uppgift 8b: {0}", demons * life / damage));
+    }
+    void Uppgift8c()
+    {
+        //Debug.Log(Random.Range(0, 5));
+        //Debug.Log(Random.Range(0f, 10f));
+        Debug.Log(string.Format("Uppgift 8c: {0}", (life / minDamage)));
+        Debug.Log(life / maxDamage);
+        Debug.Log((minDamage + maxDamage) / 2);
+    }
+    void Uppgift9()
+    {
+        count = count * 2;
+        Debug.Log(string.Format("Uppgift 9: {0}", count));
+    }
+    void Uppgift10()
+    {
+        Debug.Log(string.Format("Uppgift 10: {0}", ((countMulti * 3) / 2)));
+    }
+    void Uppgift11()
+    {
+        Debug.Log(string.Format("Uppgift 11: {0}", (playerMaxLife - enemyDamage * playerResist)));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Uppgift9();
+    }
+}
